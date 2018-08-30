5ae7485e78af314112050e9856419cef2cd2cba3
Leonardo Matos <leomp120894@gmail.com>
2018-08-30 13:29:10 -0300

    fix build script: travel subdirectories first

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 function traverse() {
   for d in "$1"/*; do
     if [ -d "$d" ]; then
+      # travel to subdirectory
+      traverse "$d"
       # run hercule
       # https://github.com/jamesramsay/hercule
       hercule $d/.hercule.md -o $d/apiary.apib
-      # travel to subdirectory
-      traverse "$d"
     fi
   done
 }
